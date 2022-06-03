52a92d1a5409615f4ee02218ff6c1c59de6d73f8
Suraj-dev-pro <52670868+Suraj-dev-pro@users.noreply.github.com>
2022-06-03 17:59:01 +0545

    Add files via upload

---
+++ merohostel.sql
@@ -0,0 +1,169 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 03, 2022 at 02:12 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `merohostel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `full_name` varchar(250) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `full_name`, `username`, `password`) VALUES
+(40, 'rabin', 'Rabin_user', 'c6f057b86584942e415435ffb1fa93d4');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking_details`
+--
+
+CREATE TABLE `booking_details` (
+  `booking_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `fullname` text NOT NULL,
+  `email` varchar(264) NOT NULL,
+  `Phone` varchar(25) NOT NULL,
+  `address` varchar(150) NOT NULL,
+  `checkin_date` date NOT NULL,
+  `hostel_id` int(50) NOT NULL,
+  `type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `Username` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `PhoneNo` varchar(20) NOT NULL,
+  `Address` varchar(100) NOT NULL,
+  `Gender` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hostels`
+--
+
+CREATE TABLE `hostels` (
+  `id` int(11) NOT NULL,
+  `image_name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `name` varchar(250) CHARACTER SET utf8 NOT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `address` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `contact` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `type` varchar(10) CHARACTER SET utf8 NOT NULL,
+  `booked` varchar(25) NOT NULL,
+  `description` text CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `hostels`
+--
+
+INSERT INTO `hostels` (`id`, `image_name`, `name`, `price`, `address`, `contact`, `type`, `booked`, `description`) VALUES
+(31, 'Hostel-Name-3930.jpg', 'Garima Hostel', '18000', 'Pulchowk', '9862331929', 'girls', 'no', '1 seater'),
+(32, 'Hostel-Name-4413.jpg', 'Bhaktapur House', '9000', 'Thimi,Bhaktapur', '9847569788', 'boys', 'no', '6 seater '),
+(33, 'Hostel-Name-9890.jpg', 'Bhibhuti Girls Hostel', '12000', 'Kupandol,Lalitpur', '9847569788', 'girls', 'yes', '3 seater'),
+(34, 'Hostel-Name-2671.jpg', 'Holmo Home', '8000', 'Boudha,Kathmadnu', '9847569788', 'boys', 'no', '2 seater'),
+(35, 'Hostel-Name-4938.jpg', 'Kusang Boys Home', '9000', 'Sankhamul,Kathmandu', '9847569788', 'boys', 'no', '3seater'),
+(36, 'Hostel-Name-6455.jpg', 'Premium Girls Home', '18000', 'Pulchowk,Lalitpur', '9862331929', 'girls', 'no', '2 seater'),
+(37, 'Hostel-Name-4596.jpg', 'Swikar Boys Hostel', '12000', 'Dharan', '9847569788', 'boys', 'no', '2 seater'),
+(38, 'Hostel-Name-6874.jpg', 'Thamel Boys House', '9000', 'Thamel,Kathmandu', '9847569788', 'boys', 'no', '2 seater');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `booking_details`
+--
+ALTER TABLE `booking_details`
+  ADD PRIMARY KEY (`booking_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `hostels`
+--
+ALTER TABLE `hostels`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `booking_details`
+--
+ALTER TABLE `booking_details`
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hostels`
+--
+ALTER TABLE `hostels`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
